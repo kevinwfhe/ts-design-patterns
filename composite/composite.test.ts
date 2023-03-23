358a43f908a6bf6c840df5ca532ae2bfcc6e1c0d
kevinwfhe <valarmorghulis_kh@outlook.com>
2023-03-22 23:10:48 -0400

    composite

---
+++ composite/composite.test.ts
@@ -0,0 +1,25 @@
+import { describe, test, expect } from "@jest/globals";
+import { Editor, Block, TextBlock } from "./text_editor";
+
+describe("Composite", () => {
+  test("Getting text content of the editor", () => {
+    const editor = new Editor();
+    editor.add(new TextBlock("Line 1"));
+    editor.add(
+      new Block([
+        new TextBlock("Line 2"),
+        new Block([new TextBlock("Line 3"), new TextBlock("Line 4")]),
+      ]),
+    );
+    expect(editor.textContent).toBe("Line 1\nLine 2\nLine 3\nLine 4");
+
+    const block = editor.resolve([1, 1, 0]) as TextBlock;
+    expect(block.text).toBe("Line 3");
+
+    block.edit("Line 3 edited");
+    expect(editor.textContent).toBe("Line 1\nLine 2\nLine 3 edited\nLine 4");
+    
+    editor.clear()
+    expect(editor.textContent).toBe("")
+  });
+});
---
+++ composite/text_editor.ts
@@ -0,0 +1,44 @@
+class Block {
+  children: Array<Block | TextBlock>;
+  constructor(children: Block["children"] = []) {
+    this.children = children;
+  }
+  add(block: Block | TextBlock) {
+    this.children.push(block);
+  }
+  get textContent() {
+    return this.children
+      .map((block) => (block instanceof Block ? block.textContent : block.text))
+      .join("\n");
+  }
+}
+
+class TextBlock {
+  text: string;
+  constructor(text: string = "") {
+    this.text = text;
+  }
+  edit(newText: string) {
+    this.text = newText;
+  }
+}
+
+class Editor extends Block {
+  constructor() {
+    super();
+  }
+  clear() {
+    this.children = [];
+  }
+  resolve(path: number[]) {
+    let block: Editor | Block | TextBlock = this;
+    for (let i = 0; i < path.length; i++) {
+      if (block instanceof Block) {
+        block = block.children[path[i]];
+      } else break;
+    }
+    return block;
+  }
+}
+
+export { Editor, Block, TextBlock };
